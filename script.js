6affbf8eb5141a0a8badb2ee37da6b36a2c99ab9
maurizio <mauriziodm91@gmail.com>
2021-08-18 00:04:20 -0300

    added input validation function

--- script.js
+++ script.js
@@ -20,6 +20,18 @@ UI.prototype.addBookToList = function(book){
     list.appendChild(row);
 };
 
+UI.prototype.showAlert = function(message, className){
+    const div = document.createElement('div');
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
 UI.prototype.clearFields = function (){
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
@@ -32,7 +44,14 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     const isbn = document.getElementById('isbn').value;
     const book = new Book(title, author, isbn);
     const ui = new UI();
-    ui.addBookToList(book);
-    ui.clearFields();
+    
+    if (title === '' || author === '' || isbn === ''){
+        ui.showAlert('Please fill in all the fields', 'error');
+    } else{
+        ui.addBookToList(book);
+        ui.showAlert('Book Added', 'success');
+        ui.clearFields();
+    }
+    
     e.preventDefault();
 });
